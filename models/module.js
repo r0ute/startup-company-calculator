7a184b87083b5f3f32a09c3f7fd1edc73af95f35
r0ute <route-a@ya.ru>
2018-07-29 00:05:58 +0300

    fix model

--- models/module.js
+++ models/module.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 var Module = mongoose.model('Module', new mongoose.Schema({
+    name: String,
+    prodTime: Number,
+    employee: String,
+    level: String,
     modules: [mongoose.Schema.ObjectId],
     components: [mongoose.Schema.ObjectId],
 }));
