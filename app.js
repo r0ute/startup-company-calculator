226c494167c06d63df34b6fc48a04dfbe99ca328
r0ute <route-a@ya.ru>
2018-07-28 23:04:31 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var componentRouter = require('./routes/components');
 
 var app = express();
 
-var mongoose = require('mongoose');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/components', componentRouter);
 
 // catch 404 and forward to error handler
