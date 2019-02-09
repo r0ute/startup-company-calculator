bd47108d6e9b2c724f07312366313da7b1f30ccb
r0ute <route-a@ya.ru>
2019-02-09 14:54:56 +0300

    remove index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
