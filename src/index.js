53ab9dd6b6608eaa0598843d3e7fb911e411ebbe
r0ute <route-a@ya.ru>
2019-02-09 18:57:44 +0300

    add ga

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === 'production') {
+    const ReactGA = require('react-ga');
+    ReactGA.initialize('UA-134201888-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
