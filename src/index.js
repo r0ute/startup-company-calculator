d9fb229f805d757c4eaa321d9d766f4dd320a4f5
r0ute <route-a@ya.ru>
2019-12-30 22:02:00 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ if (process.env.NODE_ENV === 'production') {
     ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
-if (process.env.NODE_ENV !== 'development') {
-    console.info = () => {};
-    console.debug = () => {};
-}
-
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/utils/RequirementsUtils.js
+++ src/utils/RequirementsUtils.js
@@ -15,21 +15,15 @@ class RequirementsUtils {
 
         Object.keys(componentRequirements)
             .forEach(key => {
-                console.debug("Requirement produce hours", componentRequirements[key]);
-
                 const component = Components.find(component => component.name === key);
 
                 if (allRequirements[key]) {             
                     allRequirements[key].count = allRequirements[key].count + 1 * multiplier;
-
-                    console.debug("Increased count for requirement", allRequirements[key]);
                 } else {
                     allRequirements[key] = {
                         component,
                         count: multiplier !== 1 ? multiplier : componentRequirements[key]
                     }
-
-                    console.debug("Set initial count for requirement", allRequirements[key]);
                 }
 
                 if (component.requirements) {
@@ -41,8 +35,6 @@ class RequirementsUtils {
 
                 produceHours += allRequirements[key].produceHours;
             });
-
-        console.info('Total hours', produceHours, 'requirements', componentRequirements);
 
         return produceHours;
     }
