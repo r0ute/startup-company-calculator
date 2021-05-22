c7f116c8ba66d066fb7fbe85f30cb22a25bbe746
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-22 10:36:07 +0000

    Deploy r0ute/startup-company-calculator to github.com/r0ute/startup-company-calculator.git:gh-pages

---
+++ precache-manifest.18a711ac3ef56b04b03fbe3006677fda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1961dd678c06a44cb85",
+    "url": "/startup-company-calculator/static/js/main.a1961dd6.chunk.js"
+  },
+  {
+    "revision": "107485a4c0785762b4bd",
+    "url": "/startup-company-calculator/static/js/1.107485a4.chunk.js"
+  },
+  {
+    "revision": "89512a52f003806012f1",
+    "url": "/startup-company-calculator/static/js/runtime~main.89512a52.js"
+  },
+  {
+    "revision": "fababe146e466cf318e2b11e43890685",
+    "url": "/startup-company-calculator/static/media/backend.fababe14.png"
+  },
+  {
+    "revision": "d5979cfe7eb5c8c080e41e52882ee1f8",
+    "url": "/startup-company-calculator/static/media/continuousintegration.d5979cfe.png"
+  },
+  {
+    "revision": "60c48b8fd0047b11635f4b464ec9a246",
+    "url": "/startup-company-calculator/static/media/contractagreement.60c48b8f.png"
+  },
+  {
+    "revision": "b58490e6d1e629140b70776f49878b47",
+    "url": "/startup-company-calculator/static/media/cronjob.b58490e6.png"
+  },
+  {
+    "revision": "3acbe70201d6cfee3e96f50e5ec28ebe",
+    "url": "/startup-company-calculator/static/media/processmanagement.3acbe702.png"
+  },
+  {
+    "revision": "24fa68a58c5074374cdf47e9f85850b5",
+    "url": "/startup-company-calculator/static/media/cluster.24fa68a5.png"
+  },
+  {
+    "revision": "0f905a5d1979bb144822002a3bb8430d",
+    "url": "/startup-company-calculator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/startup-company-calculator/precache-manifest.18a711ac3ef56b04b03fbe3006677fda.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/startup-company-calculator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
